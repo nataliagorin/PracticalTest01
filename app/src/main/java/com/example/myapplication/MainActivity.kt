8c2031ca60ee10e90d62b48e4e02dee9c13ae8a8
natalia.gorin <nataliastefaniagorin@gmail.com>
2024-11-06 13:06:54 +0200

    exercitii

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var count1 = 0
+    private var count2 = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val textField1 = findViewById<TextView>(R.id.textField1)
+        val textField2 = findViewById<TextView>(R.id.textField2)
+
+        button1.setOnClickListener {
+            count1++
+            textField1.text = count1.toString()
+        }
+
+        button2.setOnClickListener {
+            count2++
+            textField2.text = count2.toString()
         }
     }
 }
